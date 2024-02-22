a70f1285321e58b4c86f25d0ad847cbc9596e5ff
Joshua <jpe7619@gmail.com>
2024-02-21 19:03:54 -0800

    setup project depedencies

---
+++ src/mobilecomponents/pokedex/pokedex.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './pokedex.css';
+
+
+const Pokedex = () => {
+  return (
+    <>
+      <div className="pokedex-container">
+        Pokedex
+      </div>
+    </>
+  );
+};
+
+export default Pokedex;
